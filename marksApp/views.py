58c1ee048459499b9c0cb6ef8bf2f99c42c10117
shubham vora <shubhamvora05@gmail.com>
2021-07-11 09:49:05 +0530

    basic setup

--- marksApp/views.py
+++ marksApp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,  login, logout
+from django.contrib.auth.forms import UserChangeForm
+#from user.models import 
+#from user.forms import 
+from django.http import HttpResponse
+from django.contrib import messages
 
 # Create your views here.
+
+#userhome handling
+def userHome(request):
+    return HttpResponse("marks management system")
